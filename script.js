f9b224ae3a04afd87ecd7e1bf19c2e0d9b0e0ec7
prettyworld3 <82945711+nandish1199@users.noreply.github.com>
2022-01-09 11:50:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  alert("I am an alert box!");
+}
+
+function myFunction2() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+}
+
+function myFunction3() {
+  let text;
+  let person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+}
